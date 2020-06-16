045700c96ed060553f93916590a504229999c006
xuweizhi <624244232@qq.com>
2020-06-16 18:16:36 +0800

    spring: 初始化 bean

--- spring-context-test/src/test/java/com/spring/test/SpringTest.java
+++ spring-context-test/src/test/java/com/spring/test/SpringTest.java
@@ -4,6 +4,8 @@ import com.spring.test.bean.look_up.Display;
 import com.spring.test.module.Book;
 import com.spring.test.module.Car;
 import org.junit.Test;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -19,14 +21,21 @@ public class SpringTest {
         ApplicationContext context = new ClassPathXmlApplicationContext("classpath:XmlBeanFactory.xml");
         System.out.println("context 启动成功");
 
+        // 从 context 中取出我们的 Bean，而不是用 new MessageServiceImpl() 这种方式
+		Object car1 = context.getBean("car");
 
 		// look-up
 		Display display = (Display) context.getBean("display");
 		display.display();
 
-        // 从 context 中取出我们的 Bean，而不是用 new MessageServiceImpl() 这种方式
-        Car car = context.getBean(Car.class);
+		Car car = context.getBean(Car.class);
 		Book bean = context.getBean(Book.class);
+		DefaultListableBeanFactory factory = (DefaultListableBeanFactory) context.getAutowireCapableBeanFactory();
+
+		BeanDefinition carBean = factory.getBeanDefinition("car");
+		Object attribute = carBean.getAttribute("meta-test");
+
+		System.out.println(attribute);
 		// 这句将输出: hello world
         System.out.println(car);
     }
